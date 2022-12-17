485da900dbd3abd18469f05871fc9b7fa02b74ad
DevHeadTech <DevHeadTech@gmail.com>
2022-12-17 12:43:50 -0800

    successful day 4 - python

---
+++ 2022/day_04/python/part1.py
@@ -0,0 +1,46 @@
+#
+# @author: DevHeadTech
+# 
+# Advent of Code - Day 04
+#
+
+import sys
+
+if len(sys.argv) < 2 or len(sys.argv) > 2:
+    print('Bad Parameters')
+#
+
+def process(assignment):
+    elf1 = assignment.split(',')[0].split('-')
+    elf2 = assignment.split(',')[1].split('-')
+
+    #string conversion
+    elf1[0] = int(elf1[0])
+    elf1[1] = int(elf1[1])
+    elf2[0] = int(elf2[0])
+    elf2[1] = int(elf2[1])
+
+    if elf1[0] >= elf2[0] and elf1[0] <= elf2[1]:
+        if elf1[1] >= elf2[0] and elf1[1] <= elf2[1]:
+            return 1
+
+    if elf2[0] >= elf1[0] and elf2[0] <= elf1[1]:
+        if elf2[1] >= elf1[0] and elf2[1] <= elf1[1]:
+            return 1
+
+    
+    return 0
+#
+
+file = open(sys.argv[1], "r")
+
+total = 0 
+while ( line := file.readline().strip() ):
+    if '' != line:
+        total += process(line)
+    #
+#
+
+print("Priority Sum: ", total)
+
+file.close()
